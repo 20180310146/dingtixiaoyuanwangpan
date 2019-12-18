95ddd8a66aecf79698acc4cd3b0eac355335a039
Administrator <Administrator@jz60429>
2019-12-18 08:21:41 +0800

    自定义图像辅助方法，自定义名字过长时的辅助方法

---
+++ MVCCRUD/MVCCRUD/HtmlHelpMeth/ImgHelp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCRUD.HtmlHelpMeth
+{
+    public  static  class ImgHelp
+    {
+        public static MvcHtmlString Image(this HtmlHelper herper,string src,string alt)
+        {
+            var urlHelper = new UrlHelper(herper.ViewContext.RequestContext);
+            TagBuilder builder = new TagBuilder("img");
+            builder.MergeAttribute("src", urlHelper.Content(src));
+            builder.MergeAttribute("alt", alt);
+            builder.ToString(TagRenderMode.SelfClosing);
+            return MvcHtmlString.Create(builder.ToString());
+        }
+        public static string Truncare(this HtmlHelper herper, string mu, int lengh)
+        {
+            if (mu.Length <=lengh)
+            {
+                return mu;
+            }
+            else
+            {
+                var w = mu.Substring(0, lengh)+"...";
+                return w;
+            }
+        }
+
+    }
+}
